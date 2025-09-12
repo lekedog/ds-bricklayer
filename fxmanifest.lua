05c325e7bae1ef4bc92c0812802ab4f6e32977d5
The Ghoul <98289302+lekedog@users.noreply.github.com>
2025-09-12 15:38:07 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version "adamant"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game "rdr3"
+
+description 'Lekedog'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@rsg-core/shared/locale.lua',
+    --'locales/es.lua',
+    --'locales/*.lua',
+    'config.lua',
+}
+client_scripts {
+	'client/client.lua',
+}
+
+server_scripts {
+	'server/server.lua',
+}
+
+dependency {
+    'rsg-core',
+    'ox_lib',
+}
+
+lua54 'yes'
