97cbe4af21280c5f9af6a9987c29bfbd4af044f1
The Ghoul <98289302+lekedog@users.noreply.github.com>
2025-09-15 23:27:07 -0500

    Refactor version check functions for clarity

--- server/versionchecker.lua
+++ server/versionchecker.lua
@@ -1,11 +1,7 @@
------------------------------------------------------------------------
--- version checker
------------------------------------------------------------------------
 local function versionCheckPrint(_type, log)
     local color = _type == 'success' and '^2' or '^1'
     print(('^5[ds-bricklayer]%s %s^7'):format(color, log))
 end
-
 local function CheckVersion()
     PerformHttpRequest('https://raw.githubusercontent.com/lekedog/ds-versioncheckers/refs/heads/main/ds-bricklayer/version.txt', function(err, text, headers)
         local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
@@ -24,8 +20,4 @@ local function CheckVersion()
         end
     end)
 end
-
---------------------------------------------------------------------------------------------------
--- start version check
---------------------------------------------------------------------------------------------------
 CheckVersion()
